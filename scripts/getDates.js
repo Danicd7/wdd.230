2251b3998b859f10750dd5404081b53b87fd03cf
Dani <das22001@byui.edu>
2024-05-14 19:08:02 +0200

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -15,3 +15,19 @@ hamButton.addEventListener('click', () => {
     navButton.classList.toggle('open');
     //hamButton.classList.toggle('open');
 });
+
+// darkmode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
