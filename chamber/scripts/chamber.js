957199566beab151b7e12f51c98e12b6d5c23824
Dani <das22001@byui.edu>
2024-05-16 16:27:21 +0200

    commit

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,17 @@
+// lastModified	
+document.querySelector(
+    "#lastModified"
+).textContent = `Last Update: ${document.lastModified}`;
+
+// currentYear
+const date = new Date();
+const year = date.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
+const hamButton = document.querySelector('#menu');
+const navButton = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navButton.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
---
+++ week01/scripts/chamber.js
@@ -0,0 +1,18 @@
+// lastModified	
+document.querySelector(
+    "#lastModified"
+).textContent = `Last Update: ${document.lastModified}`;
+
+// currentYear
+const date = new Date();
+const year = date.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
+const hamButton = document.querySelector('#menu');
+const navButton = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navButton.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
